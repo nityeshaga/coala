41bf55f4947b0653c5998aef769af9d1e4791e6c
Stefan Zimmermann <zimmermann.code@gmail.com>
2017-03-21 19:05:02 +0100

    Define bear aspects the magic metaclass way
    Closes https://github.com/coala/coala-bears/issues/1517

--- tests/bears/BearTest.py
+++ tests/bears/BearTest.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 from io import BytesIO
 import multiprocessing
 import unittest
@@ -8,6 +9,7 @@ from time import sleep
 import requests
 import requests_mock
 
+from coalib.bearlib.aspects.Metadata import CommitMessage
 from coalib.bears.Bear import Bear
 from coalib.bears.BEAR_KIND import BEAR_KIND
 from coalib.bears.GlobalBear import GlobalBear
@@ -105,6 +107,25 @@ class DependentBear(Bear):
         yield w
 
 
+class AspectsBear(Bear, aspects={
+        'detect': [CommitMessage.Shortlog.ColonExistence],
+        'fix': [CommitMessage.Shortlog.TrailingPeriod],
+}):
+    pass
+
+
+class DetectOnlyAspectsBear(Bear, aspects={
+        'detect': [CommitMessage.Shortlog.ColonExistence],
+}):
+    pass
+
+
+class FixOnlyAspectsBear(Bear, aspects={
+        'fix': [CommitMessage.Shortlog.TrailingPeriod],
+}):
+    pass
+
+
 class BearTestBase(unittest.TestCase):
 
     def setUp(self):
@@ -118,6 +139,29 @@ class BearTestBase(unittest.TestCase):
 
 
 class BearTest(BearTestBase):
+
+    def test_default_aspects(self):
+        assert type(Bear.aspects) is defaultdict
+        assert Bear.aspects['detect'] == Bear.aspects['fix'] == []
+
+    def test_no_fix_aspects(self):
+        assert type(DetectOnlyAspectsBear.aspects) is defaultdict
+        assert DetectOnlyAspectsBear.aspects['fix'] == []
+        assert DetectOnlyAspectsBear.aspects['detect'] \
+            == [CommitMessage.Shortlog.ColonExistence]
+
+    def test_no_detect_aspects(self):
+        assert type(FixOnlyAspectsBear.aspects) is defaultdict
+        assert FixOnlyAspectsBear.aspects['detect'] == []
+        assert FixOnlyAspectsBear.aspects['fix'] \
+            == [CommitMessage.Shortlog.TrailingPeriod]
+
+    def test_detect_and_fix_aspects(self):
+        assert type(AspectsBear.aspects) is defaultdict
+        assert AspectsBear.aspects == {
+            'detect': [CommitMessage.Shortlog.ColonExistence],
+            'fix': [CommitMessage.Shortlog.TrailingPeriod],
+        }
 
     def test_simple_api(self):
         self.assertRaises(TypeError, TestBear, self.settings, 2)
